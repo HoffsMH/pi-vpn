88d73aab61bcb94e364cab15444ce7819e1983a1
HoffsMH <hecker.matthew@gmail.com>
2018-12-25 01:10:11 -0500

    had to wipe this repo :(

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+provider "digitalocean" {
+  token = "${var.digitalocean_token}"
+}
+
+resource "digitalocean_ssh_key" "me" {
+  name       = "me"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "digitalocean_ssh_key" "terra" {
+  name       = "terra"
+  public_key = "${file("./ssh/id_rsa.pub")}"
+}
+
+# Create a web server
+resource "digitalocean_droplet" "pi-vpn" {
+  name               = "pi-vpn"
+  image              = "debian-9-x64"
+  region             = "nyc1"
+  size               = "s-1vcpu-1gb"
+  ipv6               = true
+  private_networking = true
+
+  ssh_keys = [
+    "${digitalocean_ssh_key.me.fingerprint}",
+    "${digitalocean_ssh_key.terra.fingerprint}",
+  ]
+
+  provisioner "file" {
+    source      = "./scripts"
+    destination = "/opt/infra/"
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("./ssh/id_rsa")}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "./pki"
+    destination = "/opt/pki/"
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("./ssh/id_rsa")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /opt/infra/*",
+      "/opt/infra/install_docker.sh",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("./ssh/id_rsa")}"
+    }
+  }
+}
+
+resource "digitalocean_floating_ip" "pi-vpn-ip" {
+  droplet_id = "${digitalocean_droplet.pi-vpn.id}"
+  region     = "${digitalocean_droplet.pi-vpn.region}"
+}
+
+output "ip" {
+  value = "${digitalocean_floating_ip.pi-vpn-ip.ip_address}"
+}
