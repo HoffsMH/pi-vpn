58d34d7eb537dad5e0951c830df112cbbac15e1a
HoffsMH <hecker.matthew@gmail.com>
2019-01-06 00:05:20 -0500

    make it work

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-dcm build
+docker-compose build
 
 docker push hoffsmh/pi-vpn:latest
 
 cd ./terraform
-terraform destroy -auto-approve && terraform plan && terraform apply -auto-approve && ding && ssh pi-vpn.matthecker.com
+terraform destroy -auto-approve && terraform plan && terraform apply -auto-approve
