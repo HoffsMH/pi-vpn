160f03d761453c34b1344b744553772fe9d116fa
HoffsMH <hecker.matthew@gmail.com>
2019-05-10 09:02:59 -0500

    oops left this in :(. terraform provisioner can safely die now.

--- terraform/modules/ovpn_instance/main.tf
+++ terraform/modules/ovpn_instance/main.tf
@@ -14,36 +14,6 @@ resource "digitalocean_droplet" "main" {
   ssh_keys = [
     "${digitalocean_ssh_key.main.fingerprint}",
   ]
-
-  # provisioner "file" {
-  #   source      = "./infra"
-  #   destination = "/opt/infra"
-
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "root"
-  #     private_key = "${file("~/.ssh/id_rsa")}"
-  #   }
-  # }
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "chmod +x /opt/infra/provisioning/*",
-  #     "chmod +x /opt/infra/config/*.sh",
-
-  #     # provisioning
-  #     "/opt/infra/provisioning/install_config.sh",
-
-  #     # scheduling
-  #     "/opt/infra/provisioning/install_scheduling.sh",
-  #   ]
-
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "root"
-  #     private_key = "${file("~/.ssh/id_rsa")}"
-  #   }
-  # }
 }
 
 resource "digitalocean_floating_ip" "main" {
